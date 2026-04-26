894d92e1cbe45ce11151ae5100dbb3745dc1b239
Mohit Raj <mohitraj854334@gmail.com>
2026-04-26 15:54:43 +0530

    Time: 56 ms (6.50%) | Memory: 53.6 MB (53.18%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+
+    return obj = {
+        toBe(val2) {
+            if (val === val2)
+                return true;
+
+            else throw new Error("Not Equal");
+        }, notToBe(val2) {
+            if (val !== val2)
+                return true;
+
+            else throw new Error("Equal");
+        }
+
+
+
+    };
+};
+
+
+
+// let toBe = function (val2) {
+//     if (val === val2) {
+//         return { "value": true };
+//     }
+//     else throw new Error("Not Equal");
+// };
+// let notToBe = function (val2) {
+//     if (val !== val2) {
+//         return { "value": true };
+//     }
+//     else throw new Error("Equal");
+// };
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
