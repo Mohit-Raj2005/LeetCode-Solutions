64b4f98db171f653c11e8dea4d69471b72a2bcf5
Mohit Raj <mohitraj854334@gmail.com>
2026-06-01 15:43:37 +0530

    Time: 6 ms (43.15%) | Memory: 46.6 MB (94.81%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,19 +1,47 @@
 class Solution {
-    //this is a brute force method with O(n2) runtime complexity
+    //This solution will have runtime complexity of O(n)
+    //this is a more efficient method of solving this problem other than having a brute force method
     public int[] twoSum(int[] nums, int target) {
         int [] finalarr=new int[2];
+      int x;
+      int y;
+    
+      HashMap<Integer,Integer> Nums=new HashMap<>();
       for(int i=0;i<nums.length;i++){
-        for(int j=i+1;j<nums.length;j++){   
-            if(nums[i]+nums[j]==target){   //adding each element with the next element and then comparing it with the target
-                finalarr[0]=i;
-                finalarr[1]=j;
-            }
-
-        }
+        Nums.put(nums[i],i);
       }
-      return finalarr;
-      
+      for(int j=0;j<nums.length;j++){
+        x=nums[j];
+        y=target-x;
+        if(Nums.get(y)!=null && Nums.get(y)!=j){
+                finalarr[1]=j;
+                finalarr[0]=Nums.get(y);
+        }
+     
+       
+      }
+     return finalarr;
+          
     }
-}
 
+}
+// class Solution {
+//     //this is a brute force method with O(n2) runtime complexity
+//     public int[] twoSum(int[] nums, int target) {
+//         int [] finalarr=new int[2];
+//       for(int i=0;i<nums.length;i++){
+//         for(int j=i+1;j<nums.length;j++){   
+//             if(nums[i]+nums[j]==target){   //adding each element with the next element and then comparing it with the target
+//                 finalarr[0]=i;
+//                 finalarr[1]=j;
+//             }
+
+//         }
+//       }
+//       return finalarr;
+      
+//     }
+// }
+
+    
     
