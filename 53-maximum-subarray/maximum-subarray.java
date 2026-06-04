796811630c9fe1a80d5fa06c33ebee2e75027865
Mohit Raj <mohitraj854334@gmail.com>
2026-06-04 23:29:02 +0530

    Time: 1 ms (99.95%) | Memory: 77.1 MB (64.66%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,18 +1,17 @@
+// Solving this problem using Kadene's Algorithm
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum=0;
-        int maxsum= Integer.MIN_VALUE;
+        int sum=0; //initialising sum variable with 0
+        int maxsum= Integer.MIN_VALUE; //initialising maxsum with minimum value of integer
         for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+            sum+=nums[i];  // calculating sum till current iteration
             if(sum>maxsum){
-                maxsum=sum;
+                maxsum=sum;  //updating maxsum if the sum is greater than current sum
             }
             if(sum<0){
-                sum=0;  
+                sum=0; //making sum=0 if it is less -ve or bypassing the sum till previous interation because carrying the -ve sum will elentually decrease the sum value 
             }
-        }
-
-       
+        }  
         return maxsum;
     }
 }
