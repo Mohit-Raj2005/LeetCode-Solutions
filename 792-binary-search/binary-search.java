3005dce5317f787274236f80d465dce8ca0022f3
Mohit Raj <mohitraj854334@gmail.com>
2026-06-13 22:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (83.49%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,18 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length-1;
+        int low = 0;   //starting index
+        int high = nums.length-1;   //ending index
         while(low <= high){
-           int mid = (low + high)/2;
+        //    int mid = (low + high)/2;     this condition for calculation of mid is also valid but it can break if the mid ends up to a value greater than the range of an index
+        int mid = low + (high - low)/2; //by using this expression for the evaluation of mid will handle the edge case of overflow, overflow will not happen if we use this expression
             if(nums[mid]==target){
                 return mid;
             }
             else{   
                 if(nums[mid]<target){
-                    low = mid + 1;
+                    low = mid + 1;    //moving towards right
                 }
                 else{
-                    high = mid -1;
+                    high = mid -1;  // moving towards left
                 }
             }
             
