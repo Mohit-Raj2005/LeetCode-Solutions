de3a89192761f46aa6f9eb42a793a399ddfe0f91
Mohit Raj <mohitraj854334@gmail.com>
2026-06-16 13:47:16 +0530

    Time: 4 ms (99.24%) | Memory: 160.2 MB (26.31%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,28 +1,55 @@
+
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> lessPivot = new ArrayList<>();
-        ArrayList<Integer> inputPivot = new ArrayList<>();
-        ArrayList<Integer> morePivot = new ArrayList<>();
-        ArrayList<Integer> finalresult = new ArrayList<>();
-        int[] result = new int[nums.length];
-        for(int i=0; i<nums.length;i++){
-            if(nums[i]<pivot){
-                lessPivot.add(nums[i]);
-            }
-            else if(nums[i]>pivot){
-                morePivot.add(nums[i]);
-            }
-            else{
-                inputPivot.add(nums[i]);
+        int [] result = new int[nums.length]; //initialising the result array  
+        int index = 0;      // intialising the index
+        for(int element: nums){   // for each loop over nums array
+            if(element < pivot){  // inserting the elements lesser than the pivot element in the result array 
+                result[index] = element;
+                index++;
             }
         }
-        finalresult.addAll(lessPivot);
-        finalresult.addAll(inputPivot);
-        finalresult.addAll(morePivot);
-        for(int i=0; i<finalresult.size();i++){
-            result[i] = finalresult.get(i);
+        for(int element: nums){
+            if(element == pivot){     // inserting pivot elements in the result array
+                result[index] = element;
+                index++;
+            }
         }
-        
+        for(int element: nums){
+            if(element > pivot){    // inserting elements greater than the pivot elements in the result array
+                result[index] = element;
+                index++;
+            }
+        }
          return result;
     }
 }
+ //this solution was not the optimal solution but was accepted
+// class Solution {
+//     public int[] pivotArray(int[] nums, int pivot) {
+//         ArrayList<Integer> lessPivot = new ArrayList<>();  
+//         ArrayList<Integer> inputPivot = new ArrayList<>();
+//         ArrayList<Integer> morePivot = new ArrayList<>();
+//         ArrayList<Integer> finalresult = new ArrayList<>();   <----- finalresult list to store both the lists
+//         int[] result = new int[nums.length];
+//         for(int i=0; i<nums.length;i++){
+//             if(nums[i]<pivot){
+//                 lessPivot.add(nums[i]);   <----- list for elements lesser than the pivot element
+//             }
+//             else if(nums[i]>pivot){
+//                 morePivot.add(nums[i]);    <----- list for elements greater than the pivot element
+//             }
+//             else{
+//                 inputPivot.add(nums[i]);   <------ separate list for all the pivot elements in the input array
+//             }
+//         }
+//         finalresult.addAll(lessPivot);    <----- adding all the elements of lessPivot list to finalresult
+//         finalresult.addAll(inputPivot);  <----- adding pivot elements to the finalresult
+//         finalresult.addAll(morePivot);       <----- adding all the elements of lessPivot list to finalresult
+//         for(int i=0; i<finalresult.size();i++){   <----- iterating the finalresult list and adding it to the result array
+//             result[i] = finalresult.get(i);    
+//         }
+        
+//          return result;
+//     }
+// }
