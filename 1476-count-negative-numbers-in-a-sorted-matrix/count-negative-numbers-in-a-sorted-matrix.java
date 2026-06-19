d9e62a763ceeba056eab6f14f7dfa92b6af77a3c
Mohit Raj <mohitraj854334@gmail.com>
2026-06-19 20:00:49 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (94.99%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -1,14 +1,14 @@
 class Solution {
     public int countNegatives(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int negCount = 0;
-        int negIndx =  - 1;
-        int i =0;
+        int row = grid.length;  //number of rows
+        int col = grid[0].length;  // number of columns
+        int negCount = 0;    // total negative elements
+        int negIndx =  - 1;   // variable for storing the starting index of the negative element
+        int i =0;   // i variable for counting the rows
         while(i<row){
-            int start =0;
-            int end = grid[0].length-1;
-            while(start<=end){
+            int start =0;    // starting index for each row
+            int end = col-1;  // ending index for each row
+            while(start<=end){   // applying binary search to find the starting index of the negative element in each row
                 int mid = start + (end - start)/2;
                 if(grid[i][mid]<0){
                     negIndx = mid;
@@ -18,10 +18,10 @@ class Solution {
                     start = mid + 1;
                 }
             }
-            if(negIndx>-1){
-                negCount = negCount + grid[i].length - negIndx;
-            }
-            i++;
+            if(negIndx>-1){    // if there is negative element present 
+                negCount = negCount + col - negIndx;  // calculating the number of total negative elements in that row and adding it to the previous count of negative elements
+            } 
+            i++;  // increasing the row count
         }
         return negCount;
     }
