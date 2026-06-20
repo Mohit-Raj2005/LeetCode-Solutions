7aeb0338a123ecaab2bd459dfeb902ba7c125e8a
Mohit Raj <mohitraj854334@gmail.com>
2026-06-20 20:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 80.4 MB (38.04%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,23 +1,46 @@
 class Solution {
+    //METHOD 2
     public int peakIndexInMountainArray(int[] arr) {
         int start = 0;
-        int end = arr.length-1;
-        int mid = start + (end - start)/2;
-        while(start <= end && mid>=0){
-            
-            if(arr[mid] > arr[mid - 1]){
-                if(arr[mid] > arr[mid+1]){
-                    return mid;
-                }
-                else{
-                    start = mid + 1;
-                }
+        int end = arr.length - 1;
+        int ans = -1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] > arr[mid + 1]){
+                ans = mid;
+                end = mid -1;
             }
             else{
-                end = mid ;
+                start = mid + 1;
             }
-             mid = start + (end - start)/2;
         }
-        return 0;
+        return ans;
     }
+    
+
+
+    //METHOD 1
+    // public int peakIndexInMountainArray(int[] arr) {
+    // this solution is also accepted with runtime complexity of O(logn)
+    // runtime 0ms beats 100% and beats 20.57% in memory
+    //     int start = 0;
+    //     int end = arr.length-1;
+    //     int mid = start + (end - start)/2;
+    //     while(start <= end && mid>=0){
+            
+    //         if(arr[mid] > arr[mid - 1]){
+    //             if(arr[mid] > arr[mid+1]){
+    //                 return mid;
+    //             }
+    //             else{
+    //                 start = mid + 1;
+    //             }
+    //         }
+    //         else{
+    //             end = mid ;
+    //         }
+    //          mid = start + (end - start)/2;
+    //     }
+    //     return 0;
+    // }
 }
