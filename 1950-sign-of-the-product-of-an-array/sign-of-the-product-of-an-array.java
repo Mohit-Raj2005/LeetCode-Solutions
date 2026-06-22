6d8bf0ab98d5a5424ab935109bca4c8be4e282f0
Mohit Raj <mohitraj854334@gmail.com>
2026-06-22 09:10:38 +0530

    Time: 1 ms (27.20%) | Memory: 45.3 MB (56.00%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -1,35 +1,93 @@
 class Solution {
-    public int signFunc(int x){
-        int ans = -2;
-        if(x<0){
-            ans = -1;
-        }
-        else if(x>0){
-            ans = 1;
-        }
-        else{
-            ans = 0;
-        }
-        return ans;
-    }
-    public int arraySign(int[] nums) {
-        int product = 1;
-        for(int i =0 ; i<nums.length ; i++){
-            if(nums[i]<0){
-                nums[i] = -1;
+    public int arraySign(int[] nums){
+        int negcount = 0;
+        for(int i = 0; i < nums.length ; i++){
+            if(nums[i]==0){
+                return 0;
             }
-            if(nums[i]>0){
-                nums[i] = 1;
+            else if(nums[i]<0){
+                negcount++;
             }
             else{
                 continue;
             }
         }
-        for(int i =0; i<nums.length; i++){
-            product *= nums[i];
+        if(negcount % 2 == 0 ){
+            return 1;
         }
-
-        return signFunc(product);
-        
+        else{
+            return -1;
+        }
     }
+
+
+
+    // this is another method but can be more optimised
+    // public int arraySign(int[] nums) {
+    //     int product = 1;
+    //     for(int i=0;i<nums.length;i++){
+    //         if(nums[i]==0){
+    //             continue;
+    //         }
+    //         else if(nums[i]<0){
+    //             nums[i] = -1;
+    //         }
+    //         else{
+    //             nums[i] = 1;
+    //         }
+    //     }
+    //     for(int i =0; i<nums.length ; i++){
+    //         product *= nums[i];
+    //     }
+
+    //     if(product<0){
+    //         return -1;
+    //     }
+    //     if(product >0){
+    //         return 1;
+    //     }
+    //     return 0;
+    // }
+
+
+
+
+
+
+
+
+    //this is one solution that is accepted 
+    // public int signFunc(int x){
+    //     int ans = -2;
+    //     if(x<0){
+    //         ans = -1;
+    //     }
+    //     else if(x>0){
+    //         ans = 1;
+    //     }
+    //     else{
+    //         ans = 0;
+    //     }
+    //     return ans;
+    // }
+    // public int arraySign(int[] nums) {
+    //     int product = 1;
+    //     for(int i =0 ; i<nums.length ; i++){
+    //         if(nums[i]<0){
+    //             nums[i] = -1;
+    //         }
+    //         if(nums[i]>0){
+    //             nums[i] = 1;
+    //         }
+    //         else{
+    //             continue;
+    //         }
+    //     }
+    //     for(int i =0; i<nums.length; i++){
+    //         product *= nums[i];
+    //     }
+
+    //     return signFunc(product);
+        
+    // }
 }
