3ad356abdc553b07783e69e711bc799bf2b1e3cb
Mohit Raj <mohitraj854334@gmail.com>
2026-06-25 09:43:10 +0530

    Time: 2 ms (61.10%) | Memory: 47.5 MB (70.91%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -1,19 +1,45 @@
 class Solution {
+    //method 1
     public String firstPalindrome(String[] words) {
-        String ans = ""; // variable to store the ans string
-        for(int i =0; i< words.length; i++){
-            String rev = "";   // rev variable to store reverse string
-            for(int j = words[i].length()-1;j>=0 ; j--){
-               rev = rev + words[i].charAt(j);   // backward iterating each character in the word in the current iterarion and adding it to the reverrse string 
+        for(String word:words){
+            int start = 0;
+            int end = word.length()-1;
+            boolean ispalindrome = true;
+            while(start<=end){
+            if(word.charAt(start)!=word.charAt(end)){
+                ispalindrome = false;
+                break; 
             }
-            if(rev.equals(words[i])){   // comparing rev and the current word or checking palindrome
-                ans = ans + rev;  // storing the rev in ans if both are equal
-                break;
+            start++;
+            end--;
             }
-            else{
-                continue;
+            if(ispalindrome){
+                return word;
             }
+
         }
-        return ans;
+        return "";
     }
 }
+
+
+// method 2
+// class Solution {
+//     public String firstPalindrome(String[] words) {
+//         String ans = ""; // variable to store the ans string
+//         for(int i =0; i< words.length; i++){
+//             String rev = "";   // rev variable to store reverse string
+//             for(int j = words[i].length()-1;j>=0 ; j--){
+//                rev = rev + words[i].charAt(j);   // backward iterating each character in the word in the current iterarion and adding it to the reverrse string 
+//             }
+//             if(rev.equals(words[i])){   // comparing rev and the current word or checking palindrome
+//                 ans = ans + rev;  // storing the rev in ans if both are equal
+//                 break;
+//             }
+//             else{
+//                 continue;
+//             }
+//         }
+//         return ans;
+//     }
+// }
