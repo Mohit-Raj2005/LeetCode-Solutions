7531f93ac41198bef440b1c036724ed55ed42749
Mohit Raj <mohitraj854334@gmail.com>
2026-07-02 12:15:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (21.29%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
+        //HINT 1: given in the hints that we need to consider the representation of the given number in the base n-2.
+        //HINT 2: The number in the base (n-2) is always 12, which is not palindromic 
+        // thus for any given number in the base (n-2) will not be palindromic thus returning false will be the final solution to this problem
        return false; 
     }
 }
