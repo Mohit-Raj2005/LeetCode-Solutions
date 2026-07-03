e794662a0616783221a209f1b643009481203918
Mohit Raj <mohitraj854334@gmail.com>
2026-07-03 14:39:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (52.17%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,43 +1,45 @@
 class Solution {
+    // separate function to check whether the split is valid or not
     static boolean isValid(int[] nums, int k, int maxlength){
-        int sum = 0;
-        int splitcount = 1;
-        for(int i = 0; i< nums.length; i++){
-            if(sum + nums[i] <= maxlength){
+        int sum = 0;    // sum variable to store / compare the sum of the split subarray
+        int splitcount = 1;   // split count initialised with 1
+        for(int i = 0; i< nums.length; i++){  // iterating through the nums array to split and calculate the sum of the split subarray
+            if(sum + nums[i] <= maxlength){   // checking if the sum + nums[i] is less than the maxlength or the mid element passed 
                 sum += nums[i];
             }
             else{
-                splitcount++;
-                if( splitcount > k || nums[i] > maxlength ){
+                splitcount++;   // if it is not valid, we will increase the split count and calculate sum from further iteration 
+                if( splitcount > k || nums[i] > maxlength ){    // if the split count increases total input splits in the question, then returning false
                     return false;
                 }
                 else{
+                    // otherwise calculating sum for the next split subarray
                     sum = 0;
                     sum += nums[i];
                 }
             }
 
         }
-        return true;
+        return true;   // if the control comes out of the loop means it is a valid split and it will return true
     }
     public int splitArray(int[] nums, int k) {
-        int maxcost = 0;
+        int maxsum = 0;   // initialising maxsum 
         for(int i = 0; i<nums.length; i++){
-            maxcost += nums[i];
+            maxsum += nums[i];   // adding all the values of nums to calculate the total sum
         }
-        int start = 0;
-        int end = maxcost;
-        int ans = -1;
+        int start = 0;   // the range of cost will be from 0 to maxsum
+        int end = maxsum;   // end of the range 
+        int ans = -1;    // variable to store the final answer
         while(start <= end){
-            int mid = start + (end - start)/2;
-            if(isValid(nums , k , mid)){
-                ans = mid;
-                end = mid - 1;
+            int mid = start + (end - start)/2;     // applying binary search to find the mid of the range
+            if(isValid(nums , k , mid)){// checking if the mid number in the range can be the valid ans or not
+                ans = mid;      // if it can be a valid answer, we will store it 
+                end = mid - 1;  // and move towards left to find if any other smaller valid answer in the range
             }
             else{
-                start = mid + 1;
+                start = mid + 1;    // if it is not a valid answer then we will move towards right to look for the valid answer
             }
         }
-        return ans;
+        return ans;  // finally we will be returning the ans
     }
 }
