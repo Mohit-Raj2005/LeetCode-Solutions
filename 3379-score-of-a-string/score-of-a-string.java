e3dcd146cdb60ab0788df89a272be2448d272d0b
Mohit Raj <mohitraj854334@gmail.com>
2026-07-03 08:38:25 +0530

    Time: 1 ms (99.75%) | Memory: 43.4 MB (53.61%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,8 +1,8 @@
 class Solution {
     public int scoreOfString(String s) {
-        int ans = 0;
-        for(int i =0; i<= s.length()-2;i++){
-            ans += Math.abs(s.charAt(i) - s.charAt(i+1));
+        int ans = 0;   // variable to store the absolute answer
+        for(int i =0; i<= s.length()-2;i++){   // iterating the string 
+            ans += Math.abs(s.charAt(i) - s.charAt(i+1));   // calculating the absolute difference of the adjacent characters of the string
         }
         return ans;
     }
