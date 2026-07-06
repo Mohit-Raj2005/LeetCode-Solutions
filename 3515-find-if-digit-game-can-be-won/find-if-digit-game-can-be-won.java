2512e50e4c7a291f66bf2b151a96183358340215
Mohit Raj <mohitraj854334@gmail.com>
2026-07-07 00:09:15 +0530

    Time: 1 ms (95.72%) | Memory: 45.6 MB (19.29%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -1,18 +1,36 @@
 class Solution {
+    //========THIS IS METHOD 2============
     public boolean canAliceWin(int[] nums) {
-        int singleDigitSum = 0;  // initialising the variable to store the sum of single digits
-        int doubleDigitSum = 0;  // initialising the variable to store the sum of single digits
-        for(int i = 0; i <nums.length ; i++){   // iterating nums 
-            if(Integer.toString(nums[i]).length() == 1){   // checking for integers having single digit in the nums variable
-                singleDigitSum = singleDigitSum + nums[i];
+        int singleDigitSum = 0, doubleDigitSum = 0;  // variables to store sum 
+        for(int digit : nums){  // for each digit in nums
+            if(digit < 10){   // if it is a single digit 
+                singleDigitSum += digit;  
             }
-            else{
-               doubleDigitSum = doubleDigitSum + nums[i];
+            else{      // else if it is a double digit
+                doubleDigitSum += digit;
             }
         }
-        if(doubleDigitSum != singleDigitSum){  // checking if singleDigitSum and doubleDigitSum are not equal
-            return true;
+        if(singleDigitSum == doubleDigitSum){   // if both singleDigitSum and doubleDigitSum are equal then alice loses
+            return false;
         }
-        return false;
+        return true; //Alice wins if the sum of all single-digit numbers and the sum of all double-digit numbers are different.  || HINT ||
     }
+
+    //===========THIS IS METHOD 1============= 
+    // public boolean canAliceWin(int[] nums) {
+    //     int singleDigitSum = 0;  // initialising the variable to store the sum of single digits
+    //     int doubleDigitSum = 0;  // initialising the variable to store the sum of single digits
+    //     for(int i = 0; i <nums.length ; i++){   // iterating nums 
+    //         if(Integer.toString(nums[i]).length() == 1){   // checking for integers having single digit in the nums variable
+    //             singleDigitSum = singleDigitSum + nums[i];
+    //         }
+    //         else{
+    //            doubleDigitSum = doubleDigitSum + nums[i];
+    //         }
+    //     }
+    //     if(doubleDigitSum != singleDigitSum){  // checking if singleDigitSum and doubleDigitSum are not equal
+    //         return true;
+    //     }
+    //     return false;
+    // }
 }
