4f4072186c7aecbcba7787d12c8895567a889101
Mohit Raj <mohitraj854334@gmail.com>
2026-07-06 23:00:12 +0530

    Time: 3 ms (95.27%) | Memory: 42.5 MB (62.13%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -5,9 +5,9 @@ class Solution {
             if((i % 3==0) || (i % 5 == 0) || (i % 7==0)){    // checking for values divisible by 3 or 5 or 7
                 sum+=i;
             }
-            else{
-                continue;
-            }
+            // else{
+            //     continue;
+            // }
         }
         return sum;
     }
