bcfc88e3450a62eed11173fb6feccb7f13f578c4
Mohit Raj <mohitraj854334@gmail.com>
2026-07-10 23:47:01 +0530

    Time: 1 ms (31.99%) | Memory: 42.6 MB (98.25%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,8 +1,10 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] word = s.split(" ");  // splittig the string at " " to find words and storing it in a Array of string
-        // String last = word[word.length - 1];  // 
-        // return last.length();
-       return word[word.length - 1].length();
+        // String last = word[word.length - 1];  // storing the last word in a string
+        // return last.length();  // returning the length of the last word
+
+
+       return word[word.length - 1].length();   // we can directly return the length of the last element of the word array... this is more memory efficient 
     }
 }
