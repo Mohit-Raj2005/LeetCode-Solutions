029796e54539865488d67d40813cdfe6706772ce
Mohit Raj <mohitraj854334@gmail.com>
2026-07-11 16:48:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (23.97%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.java
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -1,10 +1,12 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
       double[] ans = new double[2];  // creating a double array of ans of 2 length to store the kelvin and farenhite
-      double kelvin = celsius + 273.15;  // calculating temperature in kelvin 
-      double Farenhite = celsius * 1.80 + 32.00; // calculating ans in farenhite
-      ans[0] = kelvin;  // adding kelvin at index 0
-      ans[1] = Farenhite; // adding farenhite at index 1 
+    //   double kelvin = celsius + 273.15;  // calculating temperature in kelvin 
+    //   double Farenhite = celsius * 1.80 + 32.00; // calculating ans in farenhite
+    //   ans[0] = kelvin;  // adding kelvin at index 0
+    //   ans[1] = Farenhite; // adding farenhite at index 1 
+    ans[0] = celsius + 273.15;
+    ans[1] =  celsius * 1.80 + 32.00;
       return ans;
     }
 }
